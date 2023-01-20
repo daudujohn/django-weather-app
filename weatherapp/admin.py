27773b69be970ba3990125f161e2e77a84304397
daudujohn <daudujoe@gmail.com>
2023-01-20 17:44:58 +0100

    Registered custom user model

--- weatherapp/admin.py
+++ weatherapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from weatherapp.models import Searchedlocation, User
 
 # Register your models here.
+admin.site.register(Searchedlocation)
+admin.site.register(User)
